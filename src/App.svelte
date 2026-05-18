4b12797daad24887e3f0209836db89bcf3f712cc
harishjayaraj10 <harish100603@gmail.com>
2026-05-18 14:35:00 +0530

    load kurals

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,14 @@
   import Navbar from './components/Navbar.svelte';
   import Home from './pages/Home.svelte';
   import Random from './pages/Random.svelte';
+  import { wrap } from 'svelte-spa-router/wrap';
 
   const routes = {
     '/': Home,
     '/random': Random,
+    '/kural': wrap({
+      asyncComponent: () => import('./pages/Kural.svelte'),
+    }),
   };
 </script>
 
